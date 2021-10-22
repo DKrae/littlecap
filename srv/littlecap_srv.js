a13051d3191614c3c1dcd3caf712823f7b7b0da7
DKrae <dan.kraemer@gmx.net>
2021-10-22 15:05:06 +0200

    connect to hdi container via user provided service

--- srv/littlecap_srv.js
+++ srv/littlecap_srv.js
@@ -1,19 +1,33 @@
-const cds = require('@sap/cds')
+const cds = require('@sap/cds');
 
 module.exports = async function() {
-    const db = await cds.connect.to('db');
+    //const db = await cds.connect.to('db');
 
     this.on('callStoredProc', async () => {
         try {
+            const xsenv = require('@sap/xsenv');
             const dbClass = require("sap-hdbext-promisfied");
-            let dbConn = new dbClass(await dbClass.createConnection(db.options.credentials));
+            let dbConn = new dbClass(await dbClass.createConnection(xsenv.readServices().sampleups.credentials));
             const hdbext = require("@sap/hdbext");
             const sp = await dbConn.loadProcedurePromisified(hdbext, null, 'SAMPLEPROC');
             const output = await dbConn.callProcedurePromisified(sp, []);
             return output.results;
-        } catch (error) {
+        } catch(error) {
             console.error(error)
             return {}
         }
+
+        // try {
+        //     const xsenv = require('@sap/xsenv');
+        //     const dbClass = require("sap-hdbext-promisfied");
+        //     let dbConn = new dbClass(await dbClass.createConnection(db.options.credentials));
+        //     const hdbext = require("@sap/hdbext");
+        //     const sp = await dbConn.loadProcedurePromisified(hdbext, null, 'SAMPLEPROC');
+        //     const output = await dbConn.callProcedurePromisified(sp, []);
+        //     return output.results;
+        // } catch (error) {
+        //     console.error(error)
+        //     return {}
+        // }
     })
 }
